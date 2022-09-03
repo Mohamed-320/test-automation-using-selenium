e90025e807c2c4b735e892e8000cc7df04188ece
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2022-09-03 23:39:58 +0200

    Created Dropdown Test

---
+++ src/test/java/base/dropdowntests/DropdownTest.java
@@ -0,0 +1,26 @@
+package base.dropdowntests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.DropdownPage;
+
+import java.util.List;
+
+public class DropdownTest extends BaseTest {
+    SoftAssert softAssert;
+    DropdownPage dropdownPage;
+
+    @Test
+    public void testDropdownSelectedOptions() {
+        String firstOption = "Option 1";
+        softAssert = new SoftAssert();
+        dropdownPage = homePage.clickOnDropDown();
+        dropdownPage.selectFromDropDown(firstOption);
+        List<String> selectedOptions = dropdownPage.getSelectedDropdownOptions();
+        softAssert.assertEquals(selectedOptions.size(), 1,
+                "Incorrect Number of The Selected Dropdown Options!");
+        softAssert.assertTrue(selectedOptions.contains(firstOption), "First dropdown option is not selected!");
+        softAssert.assertAll();
+    }
+}
