ab9cc02992877424575f50855ef8a7d1f851ac13
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2022-10-08 18:43:58 +0200

    Added Dynamic Loading Page Method

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -56,6 +56,11 @@ public class HomePage {
         return new FramesPage(driver);
     }
 
+    public DynamicLoadingPage clickOnDynamicLoading() {
+        clickOnLink("Dynamic Loading");
+        return new DynamicLoadingPage(driver);
+    }
+
     /**
      * When we create another link for each page, we'll need to create another method.
      * However, if there were (eg. 45) links when we ran that test to see how many links there were.
