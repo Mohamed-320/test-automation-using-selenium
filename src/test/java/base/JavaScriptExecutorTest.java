4c37b8ae486d05c688f74f299d0e152ed0531d69
Mohamed-320 <muhmmadabdelaziz97@gmail.com>
2022-10-08 19:36:38 +0200

    Added JavaScript Executor Test

---
+++ src/test/java/base/JavaScriptExecutorTest.java
@@ -0,0 +1,16 @@
+package base;
+
+import org.testng.annotations.Test;
+
+public class JavaScriptExecutorTest extends BaseTest {
+    @Test
+    public void testScrollToTable() {
+        homePage.clickLargeAndDeepDom().scrollToTable();
+    }
+
+    @Test
+    public void testScrollToFifthParagraph() {
+        homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+}
